e7522ef65c18b12de126ba93f1883a2439b01771
Balita1124 <ricomroatfauchard08@gmail.com>
2019-07-02 22:42:50 +0400

    save

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,7 +9,7 @@ import { HomeComponent } from './pages/home.component';
 // import { SakatiaComponent } from './pages/sakatia.component';
 import { ExcursionComponent } from './pages/excursion.component';
 import { NotFoundComponent } from './pages/commons/not-found.component';
-import { NguiInViewComponent } from './pages/common/ngui-in-view.component';
+// import { NguiInViewComponent } from './pages/common/ngui-in-view.component';
 import { AboutComponent } from './pages/about.component';
 
 @NgModule({
@@ -21,7 +21,7 @@ import { AboutComponent } from './pages/about.component';
     // SakatiaComponent,
     ExcursionComponent,
     NotFoundComponent,
-    NguiInViewComponent,
+    // NguiInViewComponent,
     AboutComponent
   ],
   imports: [
